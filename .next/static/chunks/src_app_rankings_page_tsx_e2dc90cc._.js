b4e328fa7c7a9144ba65bdab7c5bf07aaf118227
Dimas Pasha Akrilian <dimaspasha49@gmail.com>
2025-06-09 03:20:57 +0700

    Add .next/static/chunks/src_app_rankings_page_tsx_e2dc90cc._.js 1749414056930

---
+++ .next/static/chunks/src_app_rankings_page_tsx_e2dc90cc._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_rankings_page_tsx_2f5ea32c._.js"
+],
+    source: "dynamic"
+});
