11fb995aba80b1cc28dcc78c46de73c4ed049d86
Dimas Pasha Akrilian <dimaspasha49@gmail.com>
2025-06-09 03:21:34 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749414093558

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__7db745e8._.css",
+  "static/chunks/node_modules_next_6c40afc4._.js",
+  "static/chunks/src_components_5d2ac0d7._.js"
+],
+    source: "dynamic"
+});
